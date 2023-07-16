84350b16122c39e192b1ace1a3faa0744bece064
Ajay Adikari <94204251+ajayadikari@users.noreply.github.com>
2023-07-16 19:08:29 +0530

    forgot to add js file for footer functionalities
    forgot to add js file for footer functionalities in patch 6, adding them in this patch no need of patch - 6

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const resCont = document.querySelector(".section-2");
 const countriesCont = document.querySelector(".countriesSec");
-for(let i = 0; i<40; i++){
+for (let i = 0; i < 40; i++) {
     const ele = document.createElement("div");
     ele.classList.add("ele");
     const icon = `<i class="fa-solid fa-angle-right"></i>`;
     ele.innerHTML = "Hyderabad restaurant";
     ele.innerHTML += `${icon}`;
     resCont.append(ele);
-    if(i <= 20){
+    if (i <= 20) {
         const ele = document.createElement("div");
         const innerEle = document.createElement("div");
         innerEle.classList.add("innerEle");
@@ -21,7 +21,35 @@ for(let i = 0; i<40; i++){
         ele.innerHTML += `${icon}`;
         countriesCont.append(ele);
     }
-    ele.addEventListener("click", ()=>{
+    ele.addEventListener("click", () => {
         window.location.href = "restaurant-page/restaurant-page.html";
     })
 }
+
+
+
+// location box popup 
+const locBtn = document.querySelector(".locations-btn");
+console.log("working ");
+const locBox = document.querySelector(".loc-box");
+locBox.classList.add("disable");
+let locOn = false;
+for (let i = 0; i < 24; i++) {
+    const subBox = document.createElement("div");
+    subBox.classList.add("sub-box");
+    const imgEle = document.createElement("img");
+    imgEle.src = 'images/indian-flag.png';
+    subBox.append(imgEle);
+    const p = document.createElement("p");
+    p.innerHTML = `India`;
+    subBox.append(p);
+    locBox.append(subBox);
+}
+
+locBtn.addEventListener("click", () => {
+    if(locOn) locOn = false;
+    else locOn = true;
+    locBox.classList.toggle("disable");
+})
+
+
